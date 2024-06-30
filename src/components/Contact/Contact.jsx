89b576040c79a6f6fb4521a0790d7fa7290ef33a
v.ivanova <viktoriia.ivanova@vista.com>
2024-07-01 00:39:59 +0300

    added svg icons

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,3 +1,5 @@
+import { FaUser } from "react-icons/fa";
+import { FaPhone } from "react-icons/fa6";
 import css from './Contact.module.css';
 
 export default function Contact({ item, contacts, setContacts }) {
@@ -6,8 +8,8 @@ export default function Contact({ item, contacts, setContacts }) {
   }
   return (
     <li className={css.item}>
-      <div className={css.name}>{item.name}</div>
-      <div className={css.phone}>{item.phone}</div>
+      <div className={css.name}><FaUser/>{item.name}</div>
+      <div className={css.phone}><FaPhone />{item.phone}</div>
       <button className={css.btnDelete} onClick={removeItem}>
         Delete
       </button>
